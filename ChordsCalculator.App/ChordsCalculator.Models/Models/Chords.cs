2cd10e64b42217569ff202a18927ec3e30b1e135
Ariel Fiengo <ariel.fiengo@gmail.com>
2023-08-29 16:17:59 -0400

    added properties to the Chord class

--- ChordsCalculator.App/ChordsCalculator.Models/Models/Chords.cs
+++ ChordsCalculator.App/ChordsCalculator.Models/Models/Chords.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace ChordsCalculator.Models.Models
 {
-    internal class Chords
+    public class Chords
     {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public List<string> Notes { get; set; }
     }
 }
